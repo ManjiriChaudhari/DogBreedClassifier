223658d54270140557a77faf9f9500e05c840369
ManjiriChaudhari <166192801+ManjiriChaudhari@users.noreply.github.com>
2025-12-06 15:13:39 +0530

    Update check_images.py

--- check_images.py
+++ check_images.py
@@ -5,7 +5,7 @@
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
 # PROGRAMMER: Manjiri Chaudhari
 # DATE CREATED: 02/12/2025                                  
-# REVISED DATE: 02/12/2025     
+# REVISED DATE: 06/12/2025     
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
 #          summarizes how well the CNN performed on the image classification task. 
@@ -89,3 +89,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
